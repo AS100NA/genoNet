aa655dc2df96bdd1b9c84603183913229a203503
AS100NA <92155061+AS100NA@users.noreply.github.com>
2025-02-10 14:52:48 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# My ISP Website
+
+This is a basic Next.js project for your ISP website.
+
+## How to Use
+1. Upload these files to GitHub.
+2. Deploy on Vercel.
+3. Enjoy your live website!
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Your ISP</h1>
+      <p>This is a basic website for your Internet Service Provider.</p>
+    </div>
+  );
+}
